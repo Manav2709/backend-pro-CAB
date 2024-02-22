998e692f0b48c7d73a8c0fb7418ae7d31758e639
Manav2709 <manavchopra2709@gmail.com>
2024-02-22 11:56:02 +0530

    all setup configured

--- src/app.js
+++ src/app.js
@@ -15,6 +15,15 @@ app.use(cookieParser())
 
 
 
+//routes 
+import userRouter from './routes/user.routes.js'
+
+
+
+// routes declaration
+app.use("/api/v1/users", userRouter)
+
+
 
 
 export {app}
--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -1,5 +1,5 @@
 const asyncHandler = (requestHandler) => {
-    (req, res, next) => {
+    return (req, res, next) => {
         Promise.resolve(requestHandler(req, res, next)).catch((err) => next(err))
     }
 }
@@ -9,13 +9,3 @@ const asyncHandler = (requestHandler) => {
 export {asyncHandler};
 
 
-// const asyncHandler = (fn) => async (req, res, next) => {
-//     try {
-//         await  fn(req, res, next);
-//     } catch(error) {
-//         res.status(err.code || 500).json({
-//             success: false,
-//             message: err.message
-//         })
-//     }
-// }
